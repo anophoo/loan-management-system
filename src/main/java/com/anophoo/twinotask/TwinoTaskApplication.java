54f6f7f983c2f5e444e813acb2c73462f6449f1e
anophoo <ana.gurgenidze11@gmail.com>
2020-03-18 21:42:49 +0400

    Initial commit

---
+++ src/main/java/com/anophoo/twinotask/TwinoTaskApplication.java
@@ -0,0 +1,13 @@
+package com.anophoo.twinotask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwinoTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwinoTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anophoo/twinotask/TwinoTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anophoo.twinotask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwinoTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
