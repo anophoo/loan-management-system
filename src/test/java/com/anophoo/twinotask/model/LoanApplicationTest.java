03cbf96471ec1cc2fe587190adfe7113cc03584a
anophoo <ana.gurgenidze11@gmail.com>
2020-03-20 16:40:43 +0400

    Adds unit tests

--- src/test/java/com/anophoo/twinotask/model/LoanApplicationTest.java
+++ src/test/java/com/anophoo/twinotask/model/LoanApplicationTest.java
@@ -1,7 +1,29 @@
 package com.anophoo.twinotask.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-class LoanApplicationTest {
+import java.time.LocalDateTime;
+import java.time.Month;
+import org.junit.Test;
 
+public class LoanApplicationTest {
+
+  @Test
+  public void whenCreate_thenReturnLoanApplicationWithDefaultStatus() {
+    // given
+    LoanApplication loanApplication =
+        new LoanApplication(
+            "0012",
+            "Balu",
+            "MyDog",
+            LocalDateTime.of(2018, Month.AUGUST, 11, 0, 0, 0),
+            "Twino",
+            10000,
+            2500,
+            2000,
+            2);
+
+    // then
+    assertThat(loanApplication.getLoanStatus()).isEqualTo(LoanStatus.PENDING);
+  }
 }
