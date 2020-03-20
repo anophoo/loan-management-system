ba0791a63dd97e6372f90c2ce410c9bb1c341f7d
anophoo <ana.gurgenidze11@gmail.com>
2020-03-20 15:57:56 +0400

    Updates BootstrapLiveTest

--- src/test/java/com/anophoo/twinotask/SpringBootBootstrapLiveTest.java
+++ src/test/java/com/anophoo/twinotask/SpringBootBootstrapLiveTest.java
@@ -2,7 +2,6 @@ package com.anophoo.twinotask;
 
 import com.anophoo.twinotask.model.LoanApplication;
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.http.MediaType;
 
@@ -13,14 +12,18 @@ public class SpringBootBootstrapLiveTest {
 
   private LoanApplication createRandomLoanApplication() {
     LoanApplication loanApplication = new LoanApplication();
+    loanApplication.setPersonalId(RandomStringUtils.randomAlphabetic(5));
     loanApplication.setFirstName(RandomStringUtils.randomAlphabetic(5));
     loanApplication.setLastName(RandomStringUtils.randomAlphabetic(10));
     return loanApplication;
   }
 
-  private String createBookAsUri(LoanApplication book) {
-    Response response =
-        RestAssured.given().contentType(MediaType.APPLICATION_JSON_VALUE).body(book).post(API_ROOT);
+  private String createLoanAsUri(LoanApplication loanApplication) {
+    var response =
+        RestAssured.given()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(loanApplication)
+            .post(API_ROOT);
     return API_ROOT + "/" + response.jsonPath().get("id");
   }
 }
