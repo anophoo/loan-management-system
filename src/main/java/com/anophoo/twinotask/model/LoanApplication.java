f00fd6f3d94f44b4cced5d2686036ff386161bdb
anophoo <ana.gurgenidze11@gmail.com>
2020-03-20 17:34:58 +0400

    Adds LoanApplication tests checking violations

--- src/main/java/com/anophoo/twinotask/model/LoanApplication.java
+++ src/main/java/com/anophoo/twinotask/model/LoanApplication.java
@@ -4,6 +4,9 @@ import java.time.LocalDateTime;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -12,15 +15,33 @@ public class LoanApplication {
 
   @Id @GeneratedValue private Long id; // generated id
 
+  @NotBlank(message = "Personal ID is mandatory")
   private String personalId;
+
+  @NotEmpty(message = "First name is mandatory")
   private String firstName;
+
+  @NotEmpty(message = "Last name is mandatory")
   private String lastName;
+
+  @NotNull(message = "Birth date is mandatory")
   private LocalDateTime birthDate;
+
+  @NotNull(message = "Employer is mandatory")
   private String employer;
+
+  @NotNull(message = "Salary is mandatory")
   private double salary;
+
+  @NotNull(message = "Monthly liability is mandatory")
   private double liability;
+
+  @NotNull(message = "Requested amount is mandatory")
   private double requestedAmount;
+
+  @NotNull(message = "Requested term is mandatory")
   private int requestedTerm; // months
+
   private LoanStatus loanStatus;
 
   public LoanApplication() {}
--- src/test/java/com/anophoo/twinotask/model/LoanApplicationTest.java
+++ src/test/java/com/anophoo/twinotask/model/LoanApplicationTest.java
@@ -4,9 +4,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class LoanApplicationTest {
+
+  private static Validator validator;
+
+  @BeforeClass
+  public static void setupValidatorInstance() {
+    validator = Validation.buildDefaultValidatorFactory().getValidator();
+  }
 
   @Test
   public void whenCreate_thenReturnLoanApplicationWithDefaultStatus() {
@@ -26,5 +38,71 @@ public class LoanApplicationTest {
     // then
     assertThat(loanApplication.getPersonalId()).isNotNull();
     assertThat(loanApplication.getLoanStatus()).isEqualTo(LoanStatus.PENDING);
+  }
+
+  @Test
+  public void whenNotEmptyParameters_thenNoConstraintViolations() {
+    // given
+    LoanApplication loanApplication =
+        new LoanApplication(
+            "0012",
+            "Balu",
+            "MyDog",
+            LocalDateTime.of(2018, Month.AUGUST, 11, 0, 0, 0),
+            "Twino",
+            10000,
+            2500,
+            2000,
+            2);
+
+    // when
+    Set<ConstraintViolation<LoanApplication>> violations = validator.validate(loanApplication);
+
+    // then
+    assertThat(violations).isEmpty();
+  }
+
+  @Test
+  public void whenEmptyFirstName_thenOneConstraintViolation() {
+    // given
+    LoanApplication loanApplication =
+        new LoanApplication(
+            "0012",
+            "",
+            "MyDog",
+            LocalDateTime.of(2018, Month.AUGUST, 11, 0, 0, 0),
+            "Twino",
+            10000,
+            2500,
+            2000,
+            2);
+
+    // when
+    Set<ConstraintViolation<LoanApplication>> violations = validator.validate(loanApplication);
+
+    // then
+    assertThat(violations).hasSize(1);
+  }
+
+  @Test
+  public void whenNullFirstName_thenOneConstraintViolation() {
+    // given
+    LoanApplication loanApplication =
+        new LoanApplication(
+            "0012",
+            null,
+            "MyDog",
+            LocalDateTime.of(2018, Month.AUGUST, 11, 0, 0, 0),
+            "Twino",
+            10000,
+            2500,
+            2000,
+            2);
+
+    // when
+    Set<ConstraintViolation<LoanApplication>> violations = validator.validate(loanApplication);
+
+    // then
+    assertThat(violations.size()).isEqualTo(1);
   }
 }
