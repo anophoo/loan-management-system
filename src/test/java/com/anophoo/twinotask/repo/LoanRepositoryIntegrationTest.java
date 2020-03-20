4a5e12939f67498fe42ba06beb5b19fb5a074c73
anophoo <ana.gurgenidze11@gmail.com>
2020-03-20 16:28:18 +0400

    Adds tests

--- src/test/java/com/anophoo/twinotask/repo/LoanRepositoryIntegrationTest.java
+++ src/test/java/com/anophoo/twinotask/repo/LoanRepositoryIntegrationTest.java
@@ -42,4 +42,52 @@ public class LoanRepositoryIntegrationTest {
     // then
     assertThat(found.getPersonalId()).isEqualTo(loanApplication.getPersonalId());
   }
+
+  @Test
+  public void whenNotFoundByPersonalId_thenReturnNull() {
+    // given
+    entityManager.flush();
+
+    // when
+    LoanApplication found = loanRepository.findByPersonalId("12345");
+
+    // then
+    assertThat(found).isNull();
+  }
+
+  @Test
+  public void whenFindBySameFirstName_thenReturnAll() {
+    // given
+    LoanApplication loanApplication =
+        new LoanApplication(
+            "1234",
+            "TestMan",
+            "TestSurname",
+            LocalDateTime.of(1000, Month.APRIL, 26, 0, 0, 0),
+            "Twino",
+            1000,
+            1500,
+            20000,
+            12);
+    entityManager.persist(loanApplication);
+    LoanApplication loanApplication2 =
+        new LoanApplication(
+            "123453",
+            "TestMan",
+            "ASBD",
+            LocalDateTime.of(1000, Month.APRIL, 26, 0, 0, 0),
+            "Twino",
+            1000,
+            1500,
+            20000,
+            12);
+    entityManager.persist(loanApplication2);
+    entityManager.flush();
+
+    // when
+    var found = loanRepository.findByFirstName("TestMan");
+
+    // then
+    assertThat(found).hasSize(2);
+  }
 }
